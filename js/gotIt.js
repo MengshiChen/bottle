05cb597f9e1eb540bd8f5dbbbefeff8a6c74314c
MengshiChen <MengshiChen@users.noreply.github.com>
2020-04-27 01:12:07 -0700

    sketch edited

---
+++ js/gotIt.js
@@ -0,0 +1,49 @@
+'use strict';
+
+function gotData(data){
+
+
+// need to retrieve firebase data with val() method
+//this returns an object of all database
+
+  fbData = data.val();
+
+  if (fbData) {
+    console.log ('received data:');
+    console.log (fbData);
+
+    //create an array of the post values(if you need to loop through)
+    fbDataArray = Object.values(fbData);
+  } else {
+    console.log('nothing in this folder yet');
+  }
+
+
+}
+
+function errData(err){
+
+  console.log('error! did not receive data');
+  console.log(err);
+
+  //create a new node
+  // the node folder name, id, and object are all passed in as parameters
+  function createNode(_nodeFolder, _nodeID, _nodeObject){
+    firebase.database().ref(_nodeFolder + '/' + _nodeId).set(_nodeObject);
+  }
+
+  // createNode(folderName, "test", {text:"hello"});
+
+  function updateNode(_nodeFolder, _nodeID, updateObject) {
+    firebase.database().ref(_nodeFolder + '/' + _nodeID).update(_updateObject);
+  }
+
+  function deletNode(_nodeFolder, _nodeID){
+    firebase.database().ref(_nodeFolder + '/' + _nodeID).remove();
+
+  }
+
+
+
+
+}
--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,75 @@
+'use strict';
+
+
+//databaseURL: https://console.firebase.google.com/u/1/project/messageinabottle-8f3ec/database/messageinabottle-8f3ec/data
+
+let nodeData; // object we will push to firebase
+let fbData; // data we pull from firebase
+let fbDataArray; // firebase data values converted to an array
+let database; // reference to our firebase database
+let folderName = 'messages'; // name of folder you create in database
+let messageInput;
+let sendMessageBtn;
+
+
+
 function setup() {
-  createCanvas(400, 400);
+  noCanvas();
+
+  // messageInput = select ("#messageInput");
+  messageInput = document.querySelector ("#messageInput");
+  sendMessageBtn = document.querySelector ("#sendMessageBtn");
+
+  sendMessageBtn.addEventListener('click',sendMessage);
+
+  let config = {
+      apiKey: "AIzaSyCbjQyU1Di20_Ck-sC60IfAuqzM2hgY2ek",
+      authDomain: "messageinabottle-8f3ec.firebaseapp.com",
+      databaseURL: "https://messageinabottle-8f3ec.firebaseio.com",
+      projectId: "messageinabottle-8f3ec",
+      storageBucket: "messageinabottle-8f3ec.appspot.com",
+      messagingSenderId: "722089260841",
+      appId: "1:722089260841:web:f368fb761e2d49fcf36938"
+
+  };
+
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  //this references the folder you want your data to appear in
+  let ref = database.ref(folderName);
+  //folderName must be consistant across all calls to this folder
+
+  ref.on('value', gotData, errData);
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function sendMessage(){
+  if (messageInput.value){
+  let timestamp = Date.now();
+
+  nodeData = {
+
+    messageText: messageInput.value,
+    timestamp: timestamp,
+  }
+
+  console.log(messageInput.value);
+
+  // console.log (timestamp);
+  createNode(folderName, timestamp, nodeData);
+
+  createP(`send message: ${nodeData.messageText}`);
+
+  messageInput.value = ''
+
+  }else {
+  alert("uh oh. type message first")
+  }
+
 }
